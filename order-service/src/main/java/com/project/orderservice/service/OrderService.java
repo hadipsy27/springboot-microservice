18713ecbdeba4419ffe5a3e299b41b9cf6bbdefc
hadipsy27 <hadipsy27@gmail.com>
2023-04-28 10:01:23 +0700

    Order Service

---
+++ order-service/src/main/java/com/project/orderservice/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.project.orderservice.service;
+
+import com.project.orderservice.dto.OrderLineItemsDto;
+import com.project.orderservice.dto.OrderRequest;
+import com.project.orderservice.model.Order;
+import com.project.orderservice.model.OrderLineItems;
+import com.project.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        final List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(orderLineItemsDto -> mapToDto(orderLineItemsDto))
+                .toList();
+
+        order.setOrderLineItems(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
